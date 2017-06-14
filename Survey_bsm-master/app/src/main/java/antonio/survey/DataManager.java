d01175be94504a76ce8f1d95bfc3a7037eb37553
Juan Lopez <anton.lopez09@gmail.com>
2017-06-14 09:40:04 -0400

    Added app

---
+++ Survey_bsm-master/app/src/main/java/antonio/survey/DataManager.java
@@ -0,0 +1,18 @@
+package antonio.survey;
+
+/**
+ * Created by antonio on 2/16/17.
+ */
+
+public class DataManager {
+
+    public static String name, age, major, phoneNumber, emailAddress;
+
+    public static  int happiness, BibleKnowledge, believeinGod, interested;
+
+    public static  boolean info;
+
+
+
+
+}
